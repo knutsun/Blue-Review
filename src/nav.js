7c9ad4692e65e0c7a751d92eee54f831bbb5b039
Chaz Selph <csselph@gmail.com>
2020-10-24 22:52:09 -0600

    Restructure

--- src/nav.js
+++ src/nav.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './nav.css';
-import logo from './logo.png';
+import logo from './static/img/logo.png';
 
 const Nav = () => {
 
     return (
 
         <ul className="topnav">
-            <li><a className="home" href="#home"><img className="img-responsive" src={logo} alt="logo"/></a></li>
+            <li><a className="home" href="#home"><img className="img-responsive" src={logo} alt="logo which links the user to home"/></a></li>
             <li><a className="link" href="#departments">Departments</a></li>
             <li className="right"><a class="link" href="#about">About</a></li>
         </ul>
