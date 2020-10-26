b22e04ac9f329d9366defdea4e0778e028da00b2
Chaz Selph <csselph@gmail.com>
2020-10-26 03:39:34 -0600

    Rework nav

--- src/head.js
+++ src/head.js
@@ -5,7 +5,7 @@ const Head = () => {
         <head>
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <link rel="stylesheet" type="text/css" href="./nav.css" />
-            <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@500&display=swap" rel="stylesheet" />
+            <link href="https://fonts.googleapis.com/css2?family=Cousine:wght@700&display=swap" rel="stylesheet" />
         </head>
     );
 };
