e47884d4efe04fbe7efd77ae7108fff637d5de41
Chaz Selph <csselph@gmail.com>
2020-10-26 02:14:58 -0600

    Rework nav, add department icon

--- src/nav.js
+++ src/nav.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import './nav.css';
 import logo from './logo.png';
+import department_icon from "./static/img/Police-02.png";
+
 
 const Nav = () => {
 
     return (
-
-        <ul className="topnav">
-            <li><a className="home" href="#home"><img className="img-responsive" src={logo} alt="logo"/></a></li>
-            <li><a className="link" href="#departments">Departments</a></li>
-            <li className="right"><a class="link" href="#about">About</a></li>
-        </ul>
-
+      <ul className="topnav">
+        <li>
+          <a className="home" href="#home">
+            <img className="img-responsive" src={logo} alt="logo" />
+          </a>
+        </li>
+        <li>
+          <a className="link" href="#departments">
+            <img className="department-icon" src={department_icon} alt="logo" />
+            <div id="department-text">Departments</div>
+          </a>
+        </li>
+        <li className="right">
+          <a class="link" href="#about">
+            About
+          </a>
+        </li>
+      </ul>
     );
 }
 
